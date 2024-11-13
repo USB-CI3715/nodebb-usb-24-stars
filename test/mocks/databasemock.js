4c50cdb9a21207e251ab3c6f29e956a43555cc5f
anna-ayn <19-10096@usb.ve>
2024-11-13 19:51:31 -0400

    Revertir pruebas automaticas

--- test/mocks/databasemock.js
+++ test/mocks/databasemock.js
@@ -255,7 +255,6 @@ async function enableDefaultPlugins() {
 		'nodebb-plugin-dbsearch',
 		'nodebb-widget-essentials',
 		'nodebb-plugin-composer-default',
-		'nodebb-plugin-composer-question',
 		'nodebb-plugin-questions-and-answers',
 	].concat(testPlugins);
 
